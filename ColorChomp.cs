a009d0b6bf94dbbfe97d6973c74af989d1e009ba
Konesta <Konsta.suutari@gmail.com>
2016-02-25 18:28:09 +0200

    värinvaihdon testausta

--- ColorChomp.cs
+++ ColorChomp.cs
@@ -8,6 +8,9 @@ using Jypeli.Widgets;
 
 public class ColorChomp : PhysicsGame
 {
+
+    int LIIKUTUSVOIMA = 1500;
+
     public override void Begin()
     {
         Level.CreateBorders();
@@ -22,14 +25,45 @@ public class ColorChomp : PhysicsGame
     {
         PhysicsObject pelaaja = new PhysicsObject(x, y);
         pelaaja.Shape = Shape.Circle;
-        pelaaja.Mass = 10.0;
+        pelaaja.Color = Color.Blue;
         Add(pelaaja);
 
-        Keyboard.Listen(Key.Up, ButtonState.Down, Liikuta, "Liikuta pelaajaa ylöspäin", pelaaja, new Vector(1000, 0));
+        Keyboard.Listen(Key.Up, ButtonState.Down, Liikuta, "Liikuta pelaajaa ylöspäin", pelaaja, new Vector(0, LIIKUTUSVOIMA));
+        Keyboard.Listen(Key.Down, ButtonState.Down, Liikuta, "Liikuta pelaajaa alaspäin", pelaaja, new Vector(0, -LIIKUTUSVOIMA));
+        Keyboard.Listen(Key.Left, ButtonState.Down, Liikuta, "Liikuta pelaajaa vasemmalle", pelaaja, new Vector(-LIIKUTUSVOIMA, 0));
+        Keyboard.Listen(Key.Right, ButtonState.Down, Liikuta, "Liikuta pelaajaa oikealle", pelaaja, new Vector(LIIKUTUSVOIMA, 0));
+        Keyboard.Listen(Key.Space, ButtonState.Released, VaihdaVaria, "Vaihda pelaajan väriä", pelaaja);
     }
 
     public void Liikuta(PhysicsObject liikutettava, Vector suunta)
     {
         liikutettava.Push(suunta);
+    }
+
+    public void VaihdaVaria(PhysicsObject kohde)
+    {
+        bool varivaihdettu = false;
+
+        if (varivaihdettu == false && kohde.Color == Color.Blue)
+        {
+            kohde.Color = Color.Red;
+            varivaihdettu = true;
+        }
+        if (varivaihdettu == false && kohde.Color == Color.Red)
+        {
+            kohde.Color = Color.Green;
+            varivaihdettu = true;
+        }
+        if (varivaihdettu == false && kohde.Color == Color.Green)
+        {
+            kohde.Color = Color.Yellow;
+            varivaihdettu = true;
+        }
+        if (varivaihdettu == false && kohde.Color == Color.Yellow)
+        {
+            kohde.Color = Color.Blue;
+            varivaihdettu = true;
+        }
+
     }
 }
