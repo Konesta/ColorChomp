ae79fad99c2ed3c8a780623eca9654d8dd1683a5
Konesta <Konsta.suutari@gmail.com>
2016-06-30 15:00:49 +0300

    Add files via upload

---
+++ HTTPAsiakas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Sockets;
+
+/// @author Konsta Suutari
+/// @version ..2016
+/// <summary>
+/// 
+/// </summary>
+public class Teht1
+
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Main()
+    {
+        Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        s.Connect("localhost", 25000);
+
+        String snd = "GET / HTTP/1.1\r\nHost: localhost\r\n\r\n";
+
+        byte[] buffer = Encoding.ASCII.GetBytes(snd);
+
+        s.Send(buffer);
+
+        String sivu = "";
+        int count = 0;
+
+        do
+        {
+
+            byte[] rec = new byte[1024];
+
+            count = s.Receive(rec);
+            Console.Write("Tavuja vastaanotettu: " + count + "\r\n");
+            sivu += Encoding.ASCII.GetString(rec, 0, count);
+
+        } while (count > 0);
+
+        Console.Write(sivu);
+
+        Console.ReadKey();
+        s.Close();
+    }
+
+}
